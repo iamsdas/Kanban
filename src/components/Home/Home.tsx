01281b8acbb2b8f07431ac1799b2e725613798f7
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-04-19 18:57:32 +0530

    UI changes in home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,22 +21,22 @@ const Home = () => {
 
   return (
     <Layout>
-      <h1 className='py-2 text-4xl font-semibold text-gray-700 capitalize text-center'>
+      <h1 className='py-2 text-5xl font-semibold text-gray-700 capitalize text-center'>
         Welcome back,{' '}
         <span className='normal-case font-bold'>{username} !</span>
       </h1>
       <div className='flex flex-col justify-center items-center gap-8 mt-11'>
-        <div className='w-1/3 p-11 rounded-xl border-2 border-neutral-400 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
+        <div className='w-4/5 sm:w-2/3 lg:w-1/3 p-10 rounded-xl border-2 border-neutral-300 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
           <div className='text-neutral-600'>Pending Tasks</div>
-          <div className='font-bold'>{currCount}</div>
+          <div className='font-bold text-3xl'>{currCount}</div>
         </div>
-        <div className='w-1/3 p-11 rounded-xl border-2 border-neutral-400 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
+        <div className='w-4/5 sm:w-2/3 lg:w-1/3 p-10 rounded-xl border-2 border-neutral-300 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
           <div className='text-neutral-600'>Completed Tasks</div>
-          <div className='font-bold'>{completedCount}</div>
+          <div className='font-bold text-3xl'>{completedCount}</div>
         </div>
-        <div className='w-1/3 p-11 rounded-xl border-2 border-neutral-400 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
+        <div className='w-4/5 sm:w-2/3 lg:w-1/3 p-10 rounded-xl border-2 border-neutral-300 text-neutral-600 font-semibold text-2xl flex flex-col justify-center items-center'>
           <div className='text-neutral-600'>Total Tasks</div>
-          <div className='font-bold'>{totalCount}</div>
+          <div className='font-bold text-3xl'>{totalCount}</div>
         </div>
       </div>
     </Layout>
